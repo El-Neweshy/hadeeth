a63a3b2ff4f9882092844d85c837e6c4d5928c43
mahmoud.elneweshy <user@admins-Mac-mini.local>
2020-03-03 17:49:49 +0200

    adding requirements

--- utils/get_from_hadith.py
+++ utils/get_from_hadith.py
@@ -1,2 +1,7 @@
 # https://hadith.islam-db.com/single-book/146/%D8%B5%D8%AD%D9%8A%D8%AD-%D8%A7%D9%84%D8%A8%D8%AE%D8%A7%D8%B1%D9%8A/105787/7031
 
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
