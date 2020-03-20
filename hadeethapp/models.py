decef5bb5f30ac43650afdda1c43fedf64ca5a82
El-Neweshy <pet.eng.m.samir@gmail.com>
2020-03-20 13:19:15 +0200

    working on models

--- hadeethapp/models.py
+++ hadeethapp/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from hadeethapp.constants import HADEETH_BOOKS
 
-# Create your models here.
+class HadeethBook(models.Model):
+    book_name =
+    author =
+    tasneef =
+
+class TasneefRawy(models.Model):
+    mosannef =
+    rawy_adala_factor =
+    rawy_dabt_factor =
+    rawy_theqa_factor =
+
+class Sanad(models.Model):
+    rawy_name =
+    rawy_knowledge_way =   # 3n or same3a or what?
+
+
+class Rawy(models.Model):
+    rawy_name =
+    rawy_birth_date =
+    rawy_death_date =
+    rawy_tasneef = models.ForeignKey(TasneefRawy)
+
+class Hadeeth(models.Model):
+    full_hadeeth =
+    matn =
+    sanad =
+    book =
+    sanad =
+    shozoz =
+    ella =
+    tasneef =
+
+class HadeethStrength(models.Model):
+    sanad_strength =
+    hadeeth_strength =
